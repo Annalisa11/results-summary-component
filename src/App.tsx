ba54826b12f85d6fe8210b284b4cd258e474fc6a
Annalisa11 <annalisa.comin@hotmail.com>
2023-09-12 02:04:43 +0200

    styling - added general variables - experimented with scss

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import SummaryPanel from "./components/SummaryPanel";
 import MemoryIcon from "./assets/images/icon-memory.svg";
 import ReactionIcon from "./assets/images/icon-reaction.svg";
 
+import { Color } from "./components/SummaryPanel";
+
 function App() {
   return (
     <div className='app'>
@@ -17,12 +19,14 @@ function App() {
             iconPath={MemoryIcon}
             points={92}
             totalPoints={100}
+            color={Color.orange}
           />
           <SummaryPanel
             title={"Reaction"}
             iconPath={ReactionIcon}
             points={80}
             totalPoints={100}
+            color={Color.red}
           />
           <Button label={"Continue"} />
         </div>
--- src/components/SummaryPanel.tsx
+++ src/components/SummaryPanel.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./SummaryPanel.scss";
 
+export enum Color {
+  red = "red",
+  orange = "orange",
+  green = "green",
+  blue = "blue",
+}
+
 type Props = {
   title: string;
   points: number;
   totalPoints: number;
   iconPath: string;
+  color: Color;
 };
 
 const SummaryPanel = ({
@@ -13,13 +21,18 @@ const SummaryPanel = ({
   points,
   totalPoints,
   iconPath,
+  color,
 }: Props): JSX.Element => {
+  const panelStyle = {};
+
   return (
-    <div className='panel'>
-      <img src={iconPath} alt='memory icon' />
+    <div className={`panel ${color}`}>
+      <img src={iconPath} alt={`${title} icon`} />
 
       <div className='content'>
-        <div className='title'>{title}</div>
+        <div className='title' style={panelStyle}>
+          {title}
+        </div>
         <div className='points'>
           <strong>{points}</strong> / {totalPoints}
         </div>
