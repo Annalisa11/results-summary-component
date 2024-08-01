68fc52057a113abcfef57a344b3c7db789bb867f
Annalisa Comin <comin@cip.ifi.lmu.de>
2024-08-01 23:09:27 +0200

    deleted unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from "react";
-import "./App.scss";
-import JSONdata from "./data.json";
-//Components
-import Button from "./components/button";
-import SummaryPanel from "./components/SummaryPanel";
+import { useEffect, useState } from 'react';
+import './App.scss';
+import JSONdata from './data.json';
 //Assets
-import MemoryIcon from "./assets/images/icon-memory.svg";
-import ReactionIcon from "./assets/images/icon-reaction.svg";
-import VerbalIcon from "./assets/images/icon-verbal.svg";
-import VisualIcon from "./assets/images/icon-visual.svg";
+import MemoryIcon from './assets/images/icon-memory.svg';
+import ReactionIcon from './assets/images/icon-reaction.svg';
+import VerbalIcon from './assets/images/icon-verbal.svg';
+import VisualIcon from './assets/images/icon-visual.svg';
 //Types and Enums
-import { Color } from "./components/SummaryPanel/SummaryPanel";
-import Result from "./components/Result";
-import Summary from "./components/Summary";
+import { Color } from './components/SummaryPanel/SummaryPanel';
+import Result from './components/Result';
+import Summary from './components/Summary';
 
 export interface DataItem {
   category: string;
@@ -26,27 +23,24 @@ function App() {
 
   useEffect(() => {
     setData(addColorProperty(data));
-    console.log("color use effect");
-    console.log(data);
   }, []);
 
   const addColorProperty = (data: DataItem[]): DataItem[] => {
     return data.map((item) => {
-      console.log("addcolor", data);
       switch (item.category) {
-        case "Reaction":
+        case 'Reaction':
           item.color = Color.red;
           item.icon = ReactionIcon;
           break;
-        case "Memory":
+        case 'Memory':
           item.color = Color.orange;
           item.icon = MemoryIcon;
           break;
-        case "Verbal":
+        case 'Verbal':
           item.color = Color.green;
           item.icon = VerbalIcon;
           break;
-        case "Visual":
+        case 'Visual':
           item.color = Color.blue;
           item.icon = VisualIcon;
           break;
